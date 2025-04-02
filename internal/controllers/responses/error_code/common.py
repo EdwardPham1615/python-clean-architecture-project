16515fa30b4bbb809398b97b43287b68a4f92e07
edward <edwardpham1615@gmail.com>
2025-04-02 23:16:30 +0700

    add common error codes to handle unauthorized;

--- internal/controllers/responses/error_code/common.py
+++ internal/controllers/responses/error_code/common.py
@@ -12,3 +12,18 @@ common_validation_error = MessageResponse(
     msg_name="Validation Error",
     status_code=status.HTTP_400_BAD_REQUEST,
 )
+common_missing_or_invalid_token_error = MessageResponse(
+    msg_code="E003",
+    msg_name="Missing or invalid token",
+    status_code=status.HTTP_401_UNAUTHORIZED,
+)
+common_token_expired_error = MessageResponse(
+    msg_code="E004",
+    msg_name="Token Expired",
+    status_code=status.HTTP_401_UNAUTHORIZED,
+)
+common_invalid_token_error = MessageResponse(
+    msg_code="E005",
+    msg_name="Invalid token",
+    status_code=status.HTTP_401_UNAUTHORIZED,
+)
