dce49a4fa5d34792f4ce44f6bfc0b09235c748ed
edward <edwardpham1615@gmail.com>
2025-04-02 23:16:30 +0700

    add common error codes to handle unauthorized;

--- internal/controllers/responses/error_code/__init__.py
+++ internal/controllers/responses/error_code/__init__.py
@@ -4,5 +4,11 @@ from .comment import (
     get_comment_fail,
     update_comment_fail,
 )
-from .common import common_internal_error, common_validation_error
+from .common import (
+    common_internal_error,
+    common_invalid_token_error,
+    common_missing_or_invalid_token_error,
+    common_token_expired_error,
+    common_validation_error,
+)
 from .post import create_post_fail, delete_post_fail, get_post_fail, update_post_fail
