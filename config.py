6f29b66fe0a17cabbe9a17a4133d95d5bedd9cd8
edward <edwardpham1615@gmail.com>
2025-04-02 23:19:00 +0700

    add new authentication service as new infrastructure and currently support Keycloak;

--- config.py
+++ config.py
@@ -12,6 +12,16 @@ settings = Dynaconf(
 )
 
 
+class AuthenticationServiceConfig(BaseModel):
+    vendor: Optional[str] = Field("keycloak_client", alias="VENDOR")
+    url: str = Field(..., alias="URL")
+    admin_username: str = Field(..., alias="ADMIN_USERNAME")
+    admin_password: str = Field(..., alias="ADMIN_PASSWORD")
+    realm: str = Field(..., alias="REALM")
+    client_id: str = Field(..., alias="CLIENT_ID")
+    client_secret: str = Field(..., alias="CLIENT_SECRET")
+
+
 class RelationalDBConfig(BaseModel):
     vendor: Optional[str] = Field("postgres", alias="VENDOR")
     url: str = Field(..., alias="URL")
@@ -24,6 +34,10 @@ class AppConfig(BaseModel):
     uvicorn_workers: Optional[int] = Field(1, alias="UVICORN_WORKERS")
     # Relational DB configs
     relational_db: RelationalDBConfig = Field(..., alias="RELATIONAL_DB")
+    # Authentication Service configs
+    authentication_service: AuthenticationServiceConfig = Field(
+        ..., alias="AUTHENTICATION_SERVICE"
+    )
 
 
 # Load and validate configuration
@@ -36,6 +50,15 @@ try:
             VENDOR=settings.RELATIONAL_DB.VENDOR,
             URL=settings.RELATIONAL_DB.URL,
             ENABLE_AUTO_MIGRATE=settings.RELATIONAL_DB.ENABLE_AUTO_MIGRATE,
+        ),
+        AUTHENTICATION_SERVICE=AuthenticationServiceConfig(
+            VENDOR=settings.AUTHENTICATION_SERVICE.VENDOR,
+            URL=settings.AUTHENTICATION_SERVICE.URL,
+            ADMIN_USERNAME=settings.AUTHENTICATION_SERVICE.ADMIN_USERNAME,
+            ADMIN_PASSWORD=settings.AUTHENTICATION_SERVICE.ADMIN_PASSWORD,
+            REALM=settings.AUTHENTICATION_SERVICE.REALM,
+            CLIENT_ID=settings.AUTHENTICATION_SERVICE.CLIENT_ID,
+            CLIENT_SECRET=settings.AUTHENTICATION_SERVICE.CLIENT_SECRET,
         ),
     )
     logger.info("Configuration loaded successfully!")
