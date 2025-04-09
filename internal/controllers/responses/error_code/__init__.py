3ade43868274f370e738b41ee12a80be568fbb76
edward <edwardpham1615@gmail.com>
2025-04-10 00:32:25 +0700

    add invalid_webhook_secret_error;

--- internal/controllers/responses/error_code/__init__.py
+++ internal/controllers/responses/error_code/__init__.py
@@ -1,4 +1,4 @@
-from .authentication import sync_webhook_event_fail
+from .authentication import sync_webhook_event_fail, invalid_webhook_secret_error
 from .comment import (
     create_comment_fail,
     delete_comment_fail,
--- internal/controllers/responses/error_code/authentication.py
+++ internal/controllers/responses/error_code/authentication.py
@@ -7,3 +7,8 @@ sync_webhook_event_fail = MessageResponse(
     msg_name="Sync webhook event fail",
     status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
 )
+invalid_webhook_secret_error = MessageResponse(
+    msg_code="E302",
+    msg_name="Invalid webhook secret",
+    status_code=status.HTTP_401_UNAUTHORIZED,
+)
